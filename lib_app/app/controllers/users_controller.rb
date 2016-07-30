4b8bacb80af3956f7b5aa393bd2462952c2ea3c3
Indigo253931 <indigo253931@gmail.com>
2016-07-29 18:05:15 -0600

    migrations, controllers, views

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -5,4 +5,22 @@ class UsersController < ApplicationController
     render :index
   end
 
+  def new
+    # we need to make a new user to pass to the form later
+    @user = User.new
+    render :new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    @user = User.create(user_params)
+
+    redirect_to "/users"
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
 end
